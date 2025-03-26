a2b6185506f4d6308aca08677b8515a592d530e3
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-26 23:00:31 +0900

    02_datatype

--- src/Chapter_01/_01_HelloWorld.java
+++ src/Chapter_01/_01_HelloWorld.java
@@ -4,7 +4,9 @@ public class _01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
 
-
+//        System.out.print("Hello World!!");
+//        System.out.println("My Name is " + System.getProperty("user.name"));
+        // 아주 좋습니다. ^^
     }
 
 
---
+++ src/Chapter_01/_02_Data_Types.java
@@ -0,0 +1,26 @@
+package Chapter_01;
+
+// psvm 입력하면 자동으로 메인 작성 또는 main
+// sout -> System.out.println
+
+
+public class _02_Data_Types {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!!");
+        System.out.println("안녕하세요"); // 문자열 출력
+
+        System.out.println(12); // 정수형 출력
+        System.out.println(-2453);
+        System.out.println(3.4937); // 실수형
+
+        System.out.println(true); // bool 형 출력
+        System.out.println(false);
+
+        System.out.println(34 + 987); // 연산 후 계산 값 출력
+        System.out.println(1.12-0.12);
+
+    }
+}
+
