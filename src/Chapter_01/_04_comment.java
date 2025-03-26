fd1c6b4bfec9a3fbdd057e61c8cddbfebb219df9
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-26 23:20:00 +0900

    04_comment

---
+++ src/Chapter_01/_04_comment.java
@@ -0,0 +1,25 @@
+package Chapter_01;
+
+public class _04_comment {
+    public static void main(String[] args) {
+
+        // command + D를 통해 문장 복사 가능
+        /* command shift / 를 통해 여러줄 가능
+        System.out.println("너의 이름이 긴 밤을 지나 찰나가 영원이 될 때 얼마나 내가 널 좋아하면 \n 달에 네 목소리가 보여");
+        System.out.println("너의 이름이 긴 밤을 지나 찰나가 영원이 될 때 얼마나 내가 널 좋아하면 \n 달에 네 목소리가 보여");
+        System.out.println("너의 이름이 긴 밤을 지나 찰나가 영원이 될 때 얼마나 내가 널 좋아하면 \n 달에 네 목소리가 보여");
+        */
+        /*
+        * 이건 억까야
+        *  나한테 왜 이런 시련을 주시는 겁니까
+        * 아니 이건 진짜 아닌거 같습니다 ;
+        *
+        *
+        * */
+        final String CODE = "KR";
+        final String am = "ab";
+//        am = "af"; 상수를 만들 땐 final을 사용
+//        CODE = "니얼굴";
+    }
+
+}
