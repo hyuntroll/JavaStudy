59ca4068bda423a01cf0a8c542bbf0e16150f08d
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-26 23:24:12 +0900

    constatns

---
+++ src/Chapter_01/_06_constants.java
@@ -0,0 +1,14 @@
+package Chapter_01;
+
+public class _06_constants {
+
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE ="+82"; // 국가 코드
+//        KR_COUNTRY_CODE = "+8282"; 상수이기때문에 변할 수 없음
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.1415926535; // 원주율
+        final String DATE_OF_BIRTH = "2009-05-29"; // 생년 월일
+
+    }
+}
