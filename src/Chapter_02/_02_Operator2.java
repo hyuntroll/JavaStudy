ce830de0ac075d1a6b51f50d7c918758601094c5
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-27 23:21:59 +0900

    02_OPerator2

---
+++ src/Chapter_02/_02_Operator2.java
@@ -0,0 +1,17 @@
+package Chapter_02;
+
+public class _02_Operator2 {
+    public static void main(String[] args) {
+        int num;
+        num = 1; // 대입 연산자
+
+        // 복합 대입 연산자
+        num += 1;
+        num -= 2;
+        num *= 3;
+        num /= 4;
+        num %= 5;
+
+
+    }
+}
