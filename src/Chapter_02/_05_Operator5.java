816d51751a2f7ac0303d3af1aa662a112b762e13
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-27 23:28:43 +0900

    05_OPerator5

---
+++ src/Chapter_02/_05_Operator5.java
@@ -0,0 +1,13 @@
+package Chapter_02;
+
+public class _05_Operator5 {
+    public static void main(String[] args) {
+        // 삼항 연산자
+        // 결과 = 조건 ? 참일 때의 결과 : 거짓일 때의 결과
+
+        final int SUPER_NUM = (5 > 7) ? 6 : 9;
+
+        System.out.println(SUPER_NUM);
+
+    }
+}
