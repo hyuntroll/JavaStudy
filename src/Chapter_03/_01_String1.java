a369ea9b7edede63f1e4cf7ab6a7f2f25d78d47b
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-28 14:12:38 +0900

    02_String2

--- src/Chapter_03/_01_String1.java
+++ src/Chapter_03/_01_String1.java
@@ -1,4 +1,32 @@
 package Chapter_03;
 
 public class _01_String1 {
+    public static void main(String[] args) {
+        String s = "I like Python, but I don't like Java Because My Name is Fucking christan. Win Python!!";
+
+        System.out.println(s);
+
+        // 문자열의 길이
+        System.out.println(s.length());
+
+        // 대소문자 변환
+
+        // 대문자로 변경
+        System.out.println(s.toUpperCase());
+
+        // 소문자로 변경
+        System.out.println(s.toLowerCase());
+
+        // 포함 관계
+        System.out.println(s.contains("Python")); // 포함되는지
+        System.out.println(s.contains("C#"));
+        System.out.println(s.indexOf("Python")); // 위치정보 문자열의 시작 위치를 반환
+
+        System.out.println(s.indexOf("C#")); // 포함되지 않을 경우 -1
+
+        System.out.println(s.lastIndexOf("Python")); // 마지막으로 일치하는 위치정보
+        System.out.println(s.startsWith("I")); // 이 문자열로 시작하는지
+        System.out.println(s.startsWith("Python")); // 이 문자열로 시작하는지
+        System.out.println(s.endsWith(".")); // 이 문자열로 끝나는지
+    }
 }
---
+++ src/Chapter_03/_02_String2.java
@@ -0,0 +1,31 @@
+package Chapter_03;
+
+public class _02_String2 {
+    public static void main(String[] args) {
+
+        String s = "I like Python, but I don't like Java Because My Name is Fucking christan. WinPython!!";
+
+        System.out.println(s);
+
+        // 문자열 변화 ( 변수가 변하는 것은 아님 )
+        System.out.println(s.replace("Python", "C and Python")); // target을 replacement로 변환
+
+        System.out.println(s.substring(7)); // 7번째 부터 시작 ( 이전 내용은 삭제 )
+
+        System.out.println(s.substring(s.indexOf("but")));
+
+        // 시작위치는 포함하지만, 끝 위치 직전까지 잘라냄
+        System.out.println(s.substring(s.indexOf("Python"), s.lastIndexOf("Python")));
+
+        s = "      I love String.    ";
+        System.out.println(s);
+
+        System.out.println(s.trim()); // 앞뒤 공백을 제거
+
+        // 문자열 결합
+        String s1 = "i";
+        String s2 = "love u";
+        System.out.println(s1+s2);
+        System.out.println(s1.concat(s2).concat(s2).concat(s2)); // concat을 하면 괄호 안의 문자열을 결합함
+    }
+}
