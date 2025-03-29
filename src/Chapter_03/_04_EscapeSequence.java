d50a3c11bf6dd5b756cb3616b38973c8c1364682
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-29 18:49:00 +0900

    04_SecapeSequence

---
+++ src/Chapter_03/_04_EscapeSequence.java
@@ -0,0 +1,10 @@
+package Chapter_03;
+
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        // 특수 문자, 이스케이프 문자
+
+        // \n \t \\ \" \'
+        char c = '\'';
+    }
+}
