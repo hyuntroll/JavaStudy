ee350d8624156bc8e710e5158e02edac4f72aa24
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-31 20:29:07 +0900

    Quiz_05

--- src/Chapter_05/_05_ASCII.java
+++ src/Chapter_05/_05_ASCII.java
@@ -13,5 +13,23 @@ public class _05_ASCII {
         c++;
         System.out.println(c++);
         System.out.println(c);
+
+
+        String[][] seats3 = new String[10][15];
+//        String[] eng = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        char ch = 'A';
+        for (int i = 0; i < seats3.length; i++) {
+            for (int j = 0; j < seats3[i].length; j++) {
+                seats3[i][j] = String.valueOf(ch) + (j+1);
+            }
+            ch++;
+        }
+
+        for (int i = 0; i < seats3.length; i++) {
+            for (int j = 0; j < seats3[i].length; j++) {
+                System.out.print(seats3[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/Chapter_05/_Quiz_5.java
+++ src/Chapter_05/_Quiz_5.java
@@ -1,4 +1,17 @@
 package Chapter_05;
 
 public class _Quiz_5 {
+    public static void main(String[] args) {
+
+        int[] ShoesSize = new int[10];
+        ShoesSize[0] = 250;
+        for (int i = 1; i < ShoesSize.length; i++) {
+//            ShoesSize[i] = ShoesSize[i - 1] + 5;
+            ShoesSize[i] = 250 + (i * 5);
+        };
+
+        for (int Size : ShoesSize) {
+            System.out.println("사이즈 " + Size + " (재고 있음)");
+        }
+    }
 }
