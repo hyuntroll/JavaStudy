6336cb460caaa2ded3ad75c4d2febda0b9e64af8
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-31 20:33:05 +0900

    01_method

---
+++ src/Chapter_06/_01_Method.java
@@ -0,0 +1,17 @@
+package Chapter_06;
+
+public class _01_Method {
+    // 메소드 정의
+    public static void SayHello() {
+        System.out.println("안녕하시와요");
+    }
+
+
+    public static void main(String[] args) {
+        // 메소드
+
+        // 메소드 호출
+        SayHello();
+
+    }
+}
