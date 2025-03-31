1cd53c185ded46174ae014be636fffbcc4055500
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-31 20:19:25 +0900

    05_ASCII

---
+++ src/Chapter_05/_05_ASCII.java
@@ -0,0 +1,17 @@
+package Chapter_05;
+
+public class _05_ASCII {
+    public static void main(String[] args) {
+
+        char c = 'a';
+        System.out.println(c);
+        System.out.println((int)c);
+
+        c = 'b';
+        System.out.println(c);
+        System.out.println((int)c);
+        c++;
+        System.out.println(c++);
+        System.out.println(c);
+    }
+}
