49df482f855dd1419310857e4e98d94459f45112
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-04-01 14:59:56 +0900

    03_Return

---
+++ src/Chapter_06/_03_Return.java
@@ -0,0 +1,15 @@
+package Chapter_06;
+
+public class _03_Return {
+
+    public static String getPhoneNumber() {
+        String phoneNumber = "010-1234-5678";
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+            String PhoneNumber = getPhoneNumber();
+        System.out.println(PhoneNumber);
+
+    }
+}
